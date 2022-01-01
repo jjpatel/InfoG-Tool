46d3afa654be383990ec52caa6c5a167b5919592
jjpatel <67829946+jjpatel@users.noreply.github.com>
2022-01-01 10:28:21 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# InfoG-Tool
+
+
+# WhoUr
+
+WhoUr is a simple tool in python for getting info of a website and scan sqli vul with google.
+this is fast but is not powerful, actually is under construction, but is usefully.
+
+This project has been update for use python3.
+
+
+[![asciicast](https://asciinema.org/a/150443.png)](https://asciinema.org/a/150443)
+
+#### Features:
+* Whois
+* DNS Lookup
+* IP Reverse Lookup
+* Admin Finder
+* Dork Scanner
+* SQLI vul scanner
+
+## Dork scanner
+You can use you own dork(inurl:"index.php?id=") or a random dork from 'dics/dork.txt', you can save the found vulnerable sites in a text file.
+
+
+### Usage
+```
+git clone https://github.com/jopcode/whour
+cd whour
+pip3 install -r requirements.txt
+python3 whoUR.py
+```
+
+## Change-Log
+### 1.6
+[17-01-2020] retake project, Update code for use python3, Fix some bug
---
+++ whoUR.py
@@ -0,0 +1,47 @@
+import sys, os
+import argparse
+import urllib3, urllib
+import re
+
+# Modules
+from libs.colors import *
+from libs.selectChoice import select_choice
+
+Parser = argparse.ArgumentParser(prog='whoUR.py', description='Tool for information gathering')
+'''
+this has been use in the future
+Parser.add_argument('-d', '--dic-path', help='Dictonaries Path, Example: -d /root/', action='store', default='dicts/', dest='dicPath')
+Parser.add_argument('-a', '--dic-adminspage', help='Admin Page dictonary, Example: -a adminspage.txt', action='store', default='adminspage.txt', dest='dicAdminsPage')
+
+Args = Parser.parse_args()
+
+# Dictonaries
+dic_adminsPage = Args.dicPath +'/'+ Args.dicAdminsPage
+'''
+def main():
+    print('\n')
+    print(B+'             _                                ___  ')
+    print(B+' __      __ | |__     ___      _   _   _ __  |__ \ ')
+    print(B+' \ \ /\ / / | \_ \   / _ \    | | | | | |__|   / / ')
+    print(B+'  \ V  V /  | | | | | (_) |   | |_| | | |     |_|  ')
+    print(B+'   \_/\_/   |_| |_|  \___/     \__,_| |_|     (_)  ')
+    print('\n')
+    print('\n')
+
+    select_choice()
+
+    print(lG+'\n---------')
+    print(lG+'- C Y A -')
+    print(lG+'---------\n')
+    
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print(lG+'\n---------')
+        print(lG+'---------\n')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
